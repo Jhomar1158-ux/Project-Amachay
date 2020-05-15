2da9d8aad9ec3e81abd16da741eb2117961c7e89
iMarceloB <marcelo032803@gmail.com>
2020-05-15 17:07:13 -0500

    Se añadió novedades

--- app/src/main/java/com/example/amachay/pantalla_novedades.java
+++ app/src/main/java/com/example/amachay/pantalla_novedades.java
@@ -1,8 +1,16 @@
 package com.example.amachay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.amachay.includes.GlobalPage;
+import com.example.amachay.includes.NacionalPage;
+import com.example.amachay.includes.VideosPage;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class pantalla_novedades extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class pantalla_novedades extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_novedades);
+
+        //al iniciar que empiece con el fragmento nacional
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new NacionalPage()).commit();
+        }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.neg_bar);
+        bottomNavigationView.setItemIconTintList(null);// para q se vea animacion en navigationbuttom
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_nacional:
+                            fragment = new NacionalPage();
+                            break;
+                        case R.id.nav_videos:
+                            fragment = new VideosPage();
+                            break;
+                        case R.id.nav_global:
+                            fragment = new GlobalPage();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment).commit();
+                    return true;
+                }
+            };
+
 }
