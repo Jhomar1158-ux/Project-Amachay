92bd6af53f87e497722c5b16664bc527f6c6dde0
flyAgaricus <orlandomanuel1999@gmail.com>
2020-05-30 22:33:06 -0500

    Se modificó el encabezado de Diagnóstico

--- app/src/main/java/com/example/amachay/activities/pantalla_diagnostico.java
+++ app/src/main/java/com/example/amachay/activities/pantalla_diagnostico.java
@@ -7,16 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.amachay.R;
 
 //Orden: Menu -> Sintomas -> Diagnostico
 
 public class pantalla_diagnostico extends AppCompatActivity {
-    public static final String nombre = "Nicolás";          //Variable con el nombre del usuario obtenida al registrarse
+    public static String nombre = "Nicolás";          //Variable con el nombre del usuario obtenida al registrarse
     public static String color = "rojo";              //Variable con el diagnóstico obtenido de la interfaz Diagnósticos
-    public static final String malestar = "resfriado";      //Variable con el malestar, obtenido de la interfaz Diagnósticos(no terminado en esa interfaz)
-    public static final boolean dia = true;                //Variable dependiente del tiempo actual
+    public static String malestar = "resfriado";      //Variable con el malestar, obtenido de la interfaz Diagnósticos(no terminado en esa interfaz)
+    public static boolean dia = true;                //Variable dependiente del tiempo actual
     public static boolean semaforoDesplegado = false;       //dependen del programa
     public static boolean malDesplegado = false;            //dependen del programa
     @Override
@@ -25,6 +26,7 @@ public class pantalla_diagnostico extends AppCompatActivity {
         setContentView(R.layout.activity_diagnostico);
         Intent deDiagnostico = getIntent();
         color = deDiagnostico.getStringExtra("diagnostique");
+        nombre = getString(R.string.valor_nombre);
         interfaz();                                         //inicializa el fondo y los botones
         diagnosticoFinal();                                 //muestra el diagnostico
     }
@@ -48,52 +50,51 @@ public class pantalla_diagnostico extends AppCompatActivity {
         }
     }
 //Código para escribir el saludo, no está definido completamente
-
 /*
-        public void saludar(){
-            TextView saludo = (TextView) findViewById(R.id.saludo);
-            if(color == "verde"){
-                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted:");
-            }
-            else if(color == "guinda"){
-                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted es:");
-            }
-            else{
-                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted presenta:");
-            }
+
+    public void saludar(){
+        TextView saludo = (TextView) findViewById(R.id.saludo);
+        if(color == "verde"){
+            saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nal parecer:");
         }
-*/
+        else if(color == "guinda"){
+            saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted es:");
+        }
+        else{
+            saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted presenta:");
+        }
+    }*/
+
     public void diagnosticoFinal(){
-        ImageView encabezado = (ImageView) findViewById(R.id.encabezado);           //encabezado con el saludo al usuario
+                   //encabezado con el saludo al usuario
         ImageView doctor = (ImageView) findViewById(R.id.doctor);                   //imagen del doctor
         ImageView diagnosticoColor = (ImageView) findViewById(R.id.bolaDeColor);    //diagnostico, dado con una bola de color
         ImageButton cuestion = (ImageButton) findViewById(R.id.pregunta);           //boton de ?, para esconderlo cuando el diagnostico es verde
         ImageView enfermedad = (ImageView) findViewById(R.id.posibleEnfermedad);    //informacion de la posible enfermedad que se tiene (no desarrollado en Diagnosticos)
+        TextView saludo = (TextView) findViewById(R.id.saludo);
         switch(color){
             case "verde":
+                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nal parecer:");
                 if(dia){
-                    encabezado.setImageResource(R.drawable.encabezado_verde_dia);
                     doctor.setImageResource(R.drawable.doctor_verde_dia);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_verde_dia);
                     cuestion.setVisibility(View.GONE);                              //oculta el boton ?
                 }
                 else{
-                    encabezado.setImageResource(R.drawable.encabezado_verde_noche);
                     doctor.setImageResource(R.drawable.doctor_verde_noche);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_verde_noche);
                     cuestion.setVisibility(View.GONE);
                 }
                 break;
             case "amarillo":
+                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted presenta:");
                 if(dia){
-                    encabezado.setImageResource(R.drawable.encabezado_rojo_dia);
                     doctor.setImageResource(R.drawable.doctor_amarillo_noche);      //la imagen del doctor de dia cierra al programa, esta reemplazada por la nocturna
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_amarillo_dia);
                     if(malestar == "resfriado") enfermedad.setImageResource(R.drawable.informacion_resfriado_dia);      //dos posibilidades: resfriado y alergia (no desarrollado en Diagnosticos)
                     else enfermedad.setImageResource(R.drawable.informacion_alergia_dia);
                 }
                 else{
-                    encabezado.setImageResource(R.drawable.encabezado_amarillo_noche);
                     doctor.setImageResource(R.drawable.doctor_amarillo_noche);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_amarillo_noche);
                     if(malestar == "resfriado") enfermedad.setImageResource(R.drawable.informacion_resfriado_noche);    //dos posibilidades: resfriado y alergia (no desarrollado en Diagnosticos)
@@ -101,28 +102,26 @@ public class pantalla_diagnostico extends AppCompatActivity {
                 }
                 break;
             case "rojo":
+                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted presenta:");
                 if(dia){
-                    encabezado.setImageResource(R.drawable.encabezado_rojo_dia);
                     doctor.setImageResource(R.drawable.doctor_rojo_noche);          //la imagen del doctor de dia cierra al programa, esta reemplazada por la nocturna
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_rojo_dia);
                     enfermedad.setImageResource(R.drawable.informacion_gripe_dia);
                 }
                 else{
-                    encabezado.setImageResource(R.drawable.encabezado_rojo_noche);
                     doctor.setImageResource(R.drawable.doctor_rojo_noche);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_rojo_noche);
                     enfermedad.setImageResource(R.drawable.informacion_gripe_noche);
                 }
                 break;
             case "guinda":
+                saludo.setText("¡Hola "+nombre+"!\nDe acuerdo al diagnóstico,\nusted es:");
                 if(dia){
-                    encabezado.setImageResource(R.drawable.encabezado_guinda_dia);
                     doctor.setImageResource(R.drawable.doctor_guinda_dia);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_guinda_dia);
                     enfermedad.setImageResource(R.drawable.informacion_covid_dia);
                 }
                 else{
-                    encabezado.setImageResource(R.drawable.encabezado_guinda_noche);
                     doctor.setImageResource(R.drawable.doctor_guinda_noche);
                     diagnosticoColor.setImageResource(R.drawable.diagnostico_guinda_noche);
                     enfermedad.setImageResource(R.drawable.informacion_covid_noche);
